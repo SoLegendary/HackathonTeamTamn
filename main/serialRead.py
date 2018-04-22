3d11acd75c65ba6ea6c0ca04a8d63f5643074f89
Unknown <11424292@student.uts.edu.au>
2018-04-22 12:26:57 +1000

    serialRead.py milestone
    Fixed threading and serial reading, as code read last entry but arduino updated too fast, leaving python to never read received messages.

--- main/serialRead.py
+++ main/serialRead.py
@@ -11,22 +11,26 @@ def wait_on_serial():
     while True:
         serial_content.append(p.readline().rstrip(b'\n\r'))
  
-t = threading.Thread(target=wait_on_serial)
-t.daemon = True
-t.start()
+#t = threading.Thread(target=wait_on_serial)
+#t.daemon = True
+#t.start()
  
 def get_serial():
     if len(serial_content) > 0:
         return serial_content[-1]
  
 def main():
-    while True:
-        print get_serial()
-        time.sleep(0.1)
+  # while True:
+  #      print get_serial()
+  #      time.sleep(0.1)
+  p = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=5)
+  while True:
+    print p.readline().rstrip(b'\n\r')
 
          
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        t.stop()
+        #t.stop()
+        print "Done!"
